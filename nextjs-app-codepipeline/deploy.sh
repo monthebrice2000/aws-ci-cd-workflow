cf9bbf2a4e4625fc6a7c9cebf899c151e7f2ea9d
root <root@ip-172-31-1-223.us-east-2.compute.internal>
2023-06-28 19:17:37 +0000

    update deploy.sh file

--- nextjs-app-codepipeline/deploy.sh
+++ nextjs-app-codepipeline/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export NVM_DIR=$HOME/.nvm;
-. $NVM_DIR/nvm.sh;
+# export NVM_DIR=$HOME/.nvm;
+# . $NVM_DIR/nvm.sh;
 
-cd /home/ubuntu/deploy_nextjs_app
-git pull origin master
-nvm use v16
-yarn install &&
-yarn build &&
-pm2 restart next_hello_world_app
+cd /home/ubuntu/aws-ci-cd-workflow/nextjs-app-codepipeline
+sudo git pull origin master
+# nvm use v16
+sudo npm install &&
+sudo npm run build &&
+pm2 restart hello_world_nextjs
